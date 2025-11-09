449ddf4c703a5ae787dea03f2cf0fb03efc22a1a
Zenisek-GD <depalubosgerald19@gmail.com>
2025-11-10 01:05:40 +0800

    Added Item

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\V1\AuthController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
